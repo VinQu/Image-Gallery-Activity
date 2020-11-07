5ac83bfff3bd219f636d30ad8d54cfccc8bb3a1a
unknown <Vincent@DESKTOP-CSATI6S.kuikdelivery.com>
2020-11-07 13:49:25 -0500

    Added modal functionality. Still needs proper formatting.

--- image_api_request.js
+++ image_api_request.js
@@ -1,4 +1,6 @@
-fetch("https://api.unsplash.com/search/photos?page=1&query=soccer&client_id=j3X4mz-VyrokRY0-tznFwHzkZumNw7MkuzImlQc6I3k")
+var query = "Shanghai"
+
+fetch("https://api.unsplash.com/search/photos?page=1&query="+query+"&client_id=j3X4mz-VyrokRY0-tznFwHzkZumNw7MkuzImlQc6I3k")
     .then(data=>{return data.json()})
     .then(res=>{
         console.log(res)
@@ -11,7 +13,8 @@ fetch("https://api.unsplash.com/search/photos?page=1&query=soccer&client_id=j3X4
             var img = document.createElement("img");
             imgBox.getElementsByClassName("imgDiv")[i].appendChild(img).setAttribute("src",imageSize);
             var currentDiv = imgBox.getElementsByClassName("imgDiv")[i];
-            currentDiv.getElementsByTagName("img")[0].setAttribute("class","image"+i);
+            currentDiv.getElementsByTagName("img")[0].setAttribute("class","image"+i)
+            currentDiv.getElementsByTagName("img")[0].setAttribute("onclick","loadModal("+i+")");
         }
     })
 
@@ -21,7 +24,7 @@ function swapImg() {
     if (currentPage == 1) {
         console.log(currentPage)
     } else {
-        fetch("https://api.unsplash.com/search/photos?page=1&query=soccer&client_id=j3X4mz-VyrokRY0-tznFwHzkZumNw7MkuzImlQc6I3k")
+        fetch("https://api.unsplash.com/search/photos?page=1&query="+query+"&client_id=j3X4mz-VyrokRY0-tznFwHzkZumNw7MkuzImlQc6I3k")
             .then(data=>{return data.json()})
             .then(res=>{
                 console.log(res)
@@ -41,7 +44,7 @@ function swapImg2() {
     if (currentPage == 2) {
         console.log(currentPage)
     } else {
-        fetch("https://api.unsplash.com/search/photos?page=2&query=soccer&client_id=j3X4mz-VyrokRY0-tznFwHzkZumNw7MkuzImlQc6I3k")
+        fetch("https://api.unsplash.com/search/photos?page=2&query="+query+"&client_id=j3X4mz-VyrokRY0-tznFwHzkZumNw7MkuzImlQc6I3k")
             .then(data=>{return data.json()})
             .then(res=>{
                 console.log(res)
@@ -61,7 +64,7 @@ function swapImg3() {
     if (currentPage == 3) {
         console.log(currentPage)
     } else {
-        fetch("https://api.unsplash.com/search/photos?page=3&query=soccer&client_id=j3X4mz-VyrokRY0-tznFwHzkZumNw7MkuzImlQc6I3k")
+        fetch("https://api.unsplash.com/search/photos?page=3&query="+query+"&client_id=j3X4mz-VyrokRY0-tznFwHzkZumNw7MkuzImlQc6I3k")
             .then(data=>{return data.json()})
             .then(res=>{
                 console.log(res)
@@ -81,7 +84,7 @@ function swapImg4() {
     if (currentPage == 4) {
         console.log(currentPage)
     } else {
-        fetch("https://api.unsplash.com/search/photos?page=4&query=soccer&client_id=j3X4mz-VyrokRY0-tznFwHzkZumNw7MkuzImlQc6I3k")
+        fetch("https://api.unsplash.com/search/photos?page=4&query="+query+"&client_id=j3X4mz-VyrokRY0-tznFwHzkZumNw7MkuzImlQc6I3k")
             .then(data=>{return data.json()})
             .then(res=>{
                 console.log(res)
@@ -101,7 +104,7 @@ function swapImg5() {
     if (currentPage == 5) {
         console.log(currentPage)
     } else {
-        fetch("https://api.unsplash.com/search/photos?page=5&query=soccer&client_id=j3X4mz-VyrokRY0-tznFwHzkZumNw7MkuzImlQc6I3k")
+        fetch("https://api.unsplash.com/search/photos?page=5&query="+query+"&client_id=j3X4mz-VyrokRY0-tznFwHzkZumNw7MkuzImlQc6I3k")
             .then(data=>{return data.json()})
             .then(res=>{
                 console.log(res)
@@ -122,7 +125,7 @@ function swapPrev() {
         console.log(currentPage)
     } else {
         currentPage = currentPage - 1
-        fetch("https://api.unsplash.com/search/photos?page="+currentPage+"&query=soccer&client_id=j3X4mz-VyrokRY0-tznFwHzkZumNw7MkuzImlQc6I3k")
+        fetch("https://api.unsplash.com/search/photos?page="+currentPage+"&query="+query+"&client_id=j3X4mz-VyrokRY0-tznFwHzkZumNw7MkuzImlQc6I3k")
             .then(data=>{return data.json()})
             .then(res=>{
                 console.log(res)
@@ -142,7 +145,7 @@ function swapNext() {
         console.log(currentPage)
     } else {
         currentPage = currentPage + 1
-        fetch("https://api.unsplash.com/search/photos?page="+currentPage+"&query=soccer&client_id=j3X4mz-VyrokRY0-tznFwHzkZumNw7MkuzImlQc6I3k")
+        fetch("https://api.unsplash.com/search/photos?page="+currentPage+"&query="+query+"&client_id=j3X4mz-VyrokRY0-tznFwHzkZumNw7MkuzImlQc6I3k")
             .then(data=>{return data.json()})
             .then(res=>{
                 console.log(res)
@@ -156,3 +159,18 @@ function swapNext() {
             })
         }
     }
+
+function loadModal(i) {
+    var modalText = "This is image " + (i+1) + "."
+    document.body.getElementsByTagName("p")[0].innerText = modalText
+    document.body.getElementsByClassName("modal")[0].style.display = "block"
+    console.log(document.body.getElementsByClassName("imgBox")[0].getElementsByTagName("img")[i].getAttribute("class"));
+    var imgLink = document.body.getElementsByClassName("imgBox")[0].getElementsByTagName("img")[i].getAttribute("src")
+    var imgBox = document.getElementById("imgBig");
+    imgBox.setAttribute("src",imgLink)
+}
+
+function closeModal() {
+    console.log("close modal");
+    document.body.getElementsByClassName("modal")[0].style.display = "none";
+}
